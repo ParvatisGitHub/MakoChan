c6bada6c3c61afbefb8bc0405891df6f80cf24aa
ParvatisGitHub <98566804+ParvatisGitHub@users.noreply.github.com>
2022-02-10 21:51:17 -0500

    Terrain 2.0 look at these skyscrapers

--- Game/Assets/Hex Grid/Scripts/HexFeatureManager.cs
+++ Game/Assets/Hex Grid/Scripts/HexFeatureManager.cs
@@ -19,17 +19,39 @@ public class HexFeatureManager : MonoBehaviour
 	public void AddFeature(HexCell cell, Vector3 position) 
 	{
 		HexHash hash = HexMetrics.SampleHashGrid(position);
-		if (hash.a >= cell.UrbanLevel * 0.25f)
+		//if (hash.a >= cell.UrbanLevel * 0.25f)
+		//  {
+		//		return;
+		//   }
+		//Transform instance = Instantiate(urbanPrefabs[cell.UrbanLevel - 1]);
+		Transform prefab = PickPrefab(cell.UrbanLevel, hash.a, hash.b);
+		if (!prefab)
         {
 			return;
         }
-		Transform instance = Instantiate(urbanPrefabs[cell.UrbanLevel - 1]);
+		Transform instance = Instantiate(prefab);
 		position.y += instance.localScale.y * 0.5f;
 		instance.localPosition = HexMetrics.Perturb(position);
-		instance.localRotation = Quaternion.Euler(0f, 360f * hash.b, 0f);
+		instance.localRotation = Quaternion.Euler(0f, 360f * hash.c, 0f);
 		instance.SetParent(container, false);
 	}
 
 	//public Transform featurePrefab;
-	public Transform[] urbanPrefabs;
+	//public Transform[][] urbanPrefabs;
+	public HexFeatureCollection[] urbanCollections;
+	Transform PickPrefab(int level, float hash, float choice)
+	{
+		if (level > 0)
+		{
+			float[] thresholds = HexMetrics.GetFeatureThresholds(level - 1);
+			for (int i = 0; i < thresholds.Length; i++)
+			{
+				if (hash < thresholds[i])
+				{
+					return urbanCollections[i].Pick(choice);
+				}
+			}
+		}
+		return null;
+	}
 }
--- Game/Assets/Hex Grid/Scripts/HexHash.cs
+++ Game/Assets/Hex Grid/Scripts/HexHash.cs
@@ -2,15 +2,13 @@
 
 public struct HexHash {
 
-	public float a, b, c, d, e;
+	public float a, b, c;
 
 	public static HexHash Create () {
 		HexHash hash;
 		hash.a = Random.value * 0.999f;
 		hash.b = Random.value * 0.999f;
 		hash.c = Random.value * 0.999f;
-		hash.d = Random.value * 0.999f;
-		hash.e = Random.value * 0.999f;
 		return hash;
 	}
 }
