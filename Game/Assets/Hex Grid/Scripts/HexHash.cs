dc100fc7119f6809e8e3a9e39e1f1b4e948ced8a
ParvatisGitHub <98566804+ParvatisGitHub@users.noreply.github.com>
2022-02-19 13:24:55 -0500

    Update HexHash.cs
    just noted what each hash value correlates to

--- Game/Assets/Hex Grid/Scripts/HexHash.cs
+++ Game/Assets/Hex Grid/Scripts/HexHash.cs
@@ -6,34 +6,34 @@ public struct HexHash {
 
 	public static HexHash Create () {
 		HexHash hash;
-		hash.a = Random.value * 0.999f;
-		hash.b = Random.value * 0.999f;
-		hash.c = Random.value * 0.999f;
-		hash.d = Random.value * 0.999f;
-		hash.e = Random.value * 0.999f;
-		hash.f = Random.value * 0.999f;
-		hash.g = Random.value * 0.999f;
-		hash.h = Random.value * 0.999f;
-		hash.i = Random.value * 0.999f;
-		hash.j = Random.value * 0.999f;
-		hash.k = Random.value * 0.999f;
-		hash.l = Random.value * 0.999f;
-		hash.m = Random.value * 0.999f;
-		hash.n = Random.value * 0.999f;
-		hash.o = Random.value * 0.999f;
-		hash.p = Random.value * 0.999f;
-		hash.q = Random.value * 0.999f;
-		hash.r = Random.value * 0.999f;
-		hash.s = Random.value * 0.999f;
-		hash.t = Random.value * 0.999f;
-		hash.u = Random.value * 0.999f;
-		hash.v = Random.value * 0.999f;
-		hash.w = Random.value * 0.999f;
-		hash.x = Random.value * 0.999f;
-		hash.y = Random.value * 0.999f;
-		hash.z = Random.value * 0.999f;
-		hash.za = Random.value * 0.999f;
-		hash.zb = Random.value * 0.999f;
+		hash.a = Random.value * 0.999f; //Urban
+		hash.b = Random.value * 0.999f; //Farm
+		hash.c = Random.value * 0.999f; //Mine
+		hash.d = Random.value * 0.999f; //varient choice
+		hash.e = Random.value * 0.999f; // rotation
+		hash.f = Random.value * 0.999f; //jungle
+		hash.g = Random.value * 0.999f; //tforest
+		hash.h = Random.value * 0.999f; //pforest
+		hash.i = Random.value * 0.999f; //fplains
+		hash.j = Random.value * 0.999f; //forestry
+		hash.k = Random.value * 0.999f; //ice
+		hash.l = Random.value * 0.999f; //reef
+		hash.m = Random.value * 0.999f; //dock
+		hash.n = Random.value * 0.999f; //coal
+		hash.o = Random.value * 0.999f; //iron
+		hash.p = Random.value * 0.999f; //oil
+		hash.q = Random.value * 0.999f; //copper
+		hash.r = Random.value * 0.999f; //gold
+		hash.s = Random.value * 0.999f; //magicrystal
+		hash.t = Random.value * 0.999f; //magisteel
+		hash.u = Random.value * 0.999f; //uranium
+		hash.v = Random.value * 0.999f; //rubbertree
+		hash.w = Random.value * 0.999f; //aluminum
+		hash.x = Random.value * 0.999f; //lead
+		hash.y = Random.value * 0.999f; //mercury
+		hash.z = Random.value * 0.999f; //tin
+		hash.za = Random.value * 0.999f; //sulfur
+		hash.zb = Random.value * 0.999f; //niter
 		return hash;
 	}
 }
